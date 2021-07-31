ae0395935e4f9376e1dd868f40f394b0762afc2e
Raven-Blue Dragon <4637537+ravenbluedragon@users.noreply.github.com>
2021-07-31 16:05:07 +1000

    make test helper more general

---
+++ helper_test.go
@@ -0,0 +1,33 @@
+package jsongo
+
+import "testing"
+
+type logline struct {
+	format    string
+	arguments []interface{}
+}
+
+// log is a helper function to simplify testing
+func log(format string, args ...interface{}) logline {
+	return logline{format, args}
+}
+
+type argument struct {
+	format   string
+	expected interface{}
+	received interface{}
+}
+
+// helper is a function to simplify table testing
+func helper(t *testing.T, log logline, args ...argument) {
+	fail := false
+	for _, a := range args {
+		if a.expected != a.received {
+			t.Errorf(a.format, a.expected, a.received)
+			fail = true
+		}
+	}
+	if fail {
+		t.Logf(log.format, log.arguments...)
+	}
+}
